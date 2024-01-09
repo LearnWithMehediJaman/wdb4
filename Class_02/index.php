d29d36f806990f42aa1a425c54d05ad32ab5a762
Mehedi Jaman <mail4mjaman@gmail.com>
2024-01-09 23:21:03 +0600

    Class 02

---
+++ Class_02/index.php
@@ -0,0 +1,73 @@
+<?php 
+    // echo "Hello World!";
+    // print "Hello World!"."\t";
+
+    // $first_name = "John";
+    // $last_name = "Doe";
+
+    // $full_name = $first_name ." ". $last_name;
+
+    // echo $full_name."\n";
+
+    //  String  "Mehedi Jaman" " John Doe" "019149349348"
+    // Integer  1 2 3 4 5 19
+    // Floating Float    1.5 2.5 3.5 1.12
+    // Double 3.1416 3.1415926535897932384626433832795
+    // Boolean True False 0/1 High / Low 
+
+    // Container 
+    // $number1 = 25;
+    // $number2 = 13;
+
+    // $sum = $number1 + $number2;
+
+    // echo $sum."\n";
+
+    // $variable = 5;
+    // $variable2 = 12;
+
+    // $name = "Mehedi";
+
+    // $variable3 = 0;
+
+    // $variable4 = null;
+
+    // $name = "John";
+    // var_dump($name);
+
+    // $number = (float) true;
+    
+    // var_dump($number);
+
+    // $number = 12;
+
+    // echo $number."\n";
+
+    // $number = 23;
+
+
+    // echo $number."\n";
+
+    // const NUMBER = 12;
+    // echo NUMBER;
+
+    // NUMBER = 21;
+
+    // echo NUMBER;
+
+    // What is Array ?
+
+    // Array is a special variable, which can hold multiple values in one variable.
+
+    // $array = array(1,2,3,4,5,6,7,8,9,10);
+    // $array = [1,2,3,4,5,6,7,8,9,10]; //n-1
+    // $array = [['Mehedi'],2,3,4,5,6,7,8,9,10]; 
+
+    
+
+    // print_r($array);
+
+    // echo $array[0][0]."\n";
+
+    // $array = array(1,2,3,4,5,6,7,8,9,10);
+?>
