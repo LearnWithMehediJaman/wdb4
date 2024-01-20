a18eae9ada1b35fc513eeb91a71c7d943961baac
Mehedi Jaman <mail4mjaman@gmail.com>
2024-01-20 21:59:40 +0600

    - Class 05

---
+++ Class_05/index.php
@@ -0,0 +1,97 @@
+<?php 
+
+// $array1 = [1,2,3];
+// $array2 = [4,5,6];
+
+// $numbers = array_merge($array1, $array2);
+
+// print_r($numbers).PHP_EOL;
+
+// $array = [1,2,3,4,5,6,7];
+
+// print_r($array);
+
+// array_pop($array);
+
+// print_r($array);
+
+// array_push($array, 8, 9, 10, 11);
+
+// print_r($array);
+
+// function myfunction($v1,$v2)
+// {
+//     return $v1 . "-" . $v2;
+// }
+
+// $animals = ["Dog", "Cat", "Horse",  "Tiger"];
+
+// print_r(array_reduce($animals,"myfunction"));
+
+
+
+// $numbers = [1,2,4,56,34,42,2,32,23,232,23, 34,34,34,34,23,23,45.343];
+
+// function sum($e1, $e2)
+// {
+//     return $e1 + $e2;
+// }
+
+// $sum = array_reduce($numbers, "sum");
+
+// var_dump($sum);
+
+
+// $array1 = [1,2,4];
+// $array2 = [5,6,8];
+
+// $array3 = array_replace($array1, $array2);
+
+// print_r($array3);
+// // print_r($array2);
+
+// print_r($array1);
+// print_r(array_reverse($array1));
+
+
+// $numbers = [8,7,6,5,4,3,2,1];
+// array_reverse($numbers);
+
+// $numbers = [8,7,6,5,3,2,1];
+
+// if($result = array_search(4, $numbers)){
+//     echo "Element Found !".PHP_EOL;
+// }
+// else{
+//     echo "Not Found".PHP_EOL;
+// }
+// array_shift($numbers);
+
+// print_r($numbers);
+
+// $numbers = [8,7,6,5,3,2,1];
+// $reversed_array = array_reverse($numbers);
+// $new_array = array_slice($reversed_array, 3);
+// $reversed_array = array_reverse($new_array);
+
+// print_r($reversed_array);
+
+
+// $numbers = [8,7,6,5,3,2,1];
+
+// echo array_sum($numbers);
+
+// $numbers = [8,7,6,5,3,2,1];
+// echo $numbers[0];
+
+// $numbers = ['First' => 8, 'Second' => 7, 'Third' => 6];
+// echo $numbers['second'].PHP_EOL;
+
+// $new_array = array_change_key_case($numbers, CASE_LOWER);
+
+// print_r($new_array);
+
+
+// $numbers = [8,7,6,5,3,2,1];
+
+// print_r(array_chunk($numbers, 3));
