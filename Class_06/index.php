1a71fef3b1008d82b197dc6c6f25d4e57b1a7eeb
Mehedi Jaman <mail4mjaman@gmail.com>
2024-01-23 21:59:52 +0600

    Class 06

---
+++ Class_06/index.php
@@ -0,0 +1,68 @@
+<?php 
+// while
+// do while
+// for
+// foreach
+
+// break
+// continue
+
+// $number = 1;
+// $status = true;
+// while($status == true){
+//     echo "Mehedi Jaman".PHP_EOL;
+
+//     $status = false;
+// }
+
+$number = 2;
+
+// while($number < 100){
+    
+//     if($number % 2 != 0){
+//         $number++;
+//         continue;
+//     }
+//     echo $number.PHP_EOL;
+
+//     $number++;
+// }
+
+// while($number < 10):
+    
+//     if($number % 2 != 0){
+//         $number++;
+//         continue;
+//     }
+//     echo $number.PHP_EOL;
+
+//     $number++;
+// endwhile;
+
+// do while
+// $number = 1;
+// do{
+//     echo "Mehedi Jaman".PHP_EOL;
+//     $number++;
+// }while($number < 10);
+
+// for loop
+
+// for($number = 1; $number < 10; $number++){
+//     if($number == 5){
+//         continue;
+//     }
+//     echo $number.PHP_EOL;
+// }
+
+// foreach loop
+$persons = ['Mehedi', 'Jaman', 'Rahman'];
+
+
+// for($index = 0; $index < count($persons); $index++){
+//     echo $persons[$index].PHP_EOL;
+// }
+
+// foreach($persons as $person){
+//     echo $person.PHP_EOL;
+// }
