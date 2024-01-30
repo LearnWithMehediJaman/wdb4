a4402965266804e8acadd855ddba9fb41260b20d
Mehedi Jaman <mail4mjaman@gmail.com>
2024-01-30 21:55:41 +0600

    - Class 07 - PHP Functions

---
+++ Class_07/index.php
@@ -0,0 +1,49 @@
+<?php
+// $n1 = 12;
+// $n2 = 34;
+
+// function sum($n1 = 0, $n2 = 0) : array
+// {
+//     return [];
+// }
+
+
+// $result = sum();
+
+// echo $result.PHP_EOL;
+
+// function greet()
+// {
+//     echo ("Hello, World". PHP_EOL);
+// }
+
+// greet();
+
+// Method / Function 
+// Built in function
+
+// User defined function
+
+
+// $n = 12;
+
+// function show()
+// {
+//     $n = 10;
+
+//     echo "n = $n". PHP_EOL;
+// }
+
+// echo $n;
+// show();
+
+// function greet($name)
+// {
+//     echo "Hello, $name". PHP_EOL;
+// }
+
+// $name = "Mehedi";
+
+
+
+// greet($name);
