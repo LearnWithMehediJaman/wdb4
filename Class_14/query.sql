4da32d5784519f8d0b1f0d8cce6c9e73f293339d
Mehedi Jaman <mail4mjaman@gmail.com>
2024-02-26 22:06:00 +0600

    - Class 14 done

---
+++ Class_14/query.sql
@@ -0,0 +1,65 @@
+-- create database query
+-- CREATE DATABASE student_management_sytem;
+
+-- delete database query
+-- DROP DATABASE student_management_sytem;
+
+-- Select Default Database Query
+-- USE student_management_sytem;
+
+-- Table Create Query
+-- CREATE TABLE students (
+-- id int auto_increment primary key, 
+-- class_roll varchar(7), 
+-- board_roll varchar(25), 
+-- reg_no varchar(25),
+-- name varchar(255),
+-- dob date,
+-- gender varchar(10),
+-- father_name varchar(255),
+-- mother_name varchar(255),
+-- student_phone varchar(15),
+-- father_phone varchar(15),
+-- mother_phone varchar(15)
+-- );
+
+
+-- Get all columns of a table
+-- SELECT * FROM students;
+
+-- Get selected columns from a table
+-- SELECT 
+-- students.name, 
+-- students.father_name, 
+-- students.student_phone, 
+-- students.father_phone 
+-- FROM students;
+
+-- Insert data into table
+-- INSERT INTO students (
+-- class_roll, 
+-- board_roll, 
+-- reg_no, 
+-- name, 
+-- dob, 
+-- gender, 
+-- father_name, 
+-- mother_name, 
+-- student_phone, 
+-- father_phone, 
+-- mother_phone
+-- )
+
+-- VALUES(
+-- '2485001',
+-- '248501',
+-- '98765432',
+-- 'John Doe',
+-- '2002-01-01',
+-- 'male',
+-- 'Jacob Doe',
+-- 'Jessy Doe',
+-- '01914090747',
+-- '01557005659',
+-- '01914090747'
+-- )
